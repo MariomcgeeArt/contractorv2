3a4ed9af2c9859c6bb2972520203a6a85a1f85a1
Mario McGee <mario.mcgee@students.makeschool.com>
2019-12-10 22:33:50 -0800

    third commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.bracelet
@@ -32,6 +32,40 @@ def bracelet_submit():
     bracelets.insert_one(bracelet)# may need to be bracelets
     #print(request.form.to_dict())
     return redirect(url_for('bracelets_index'))
+
+
+
+@app.route('/bracelets/<bracelet_id>')
+def bracelets_detail(bracelet_id):
+    """Show a single playlist."""
+    bracelet = bracelets.find_one({'_id': ObjectId(bracelet_id)})
+    return render_template('bracelets_detail.html', bracelet = bracelet)
+
+@app.route('/bracelets/<bracelet_id>', methods=['POST'])
+def bracelets_update(bracelet_id):
+    """Submit an edited bracelet."""
+    
+    # create our updated playlist
+    updated_bracelet = {
+        'brand': request.form.get('brand'),
+        'size': request.form.get('size'),
+        'image': request.form.get("image"),
+        'price': request.form.get("price")    
+    }
+    # set the former playlist to the new one we just updated/edited
+    bracelets.update_one(
+        {'_id': ObjectId(bracelet_id)},
+        {'$set': updated_bracelet})
+    # take us back to the playlist's show page
+    return redirect(url_for('bracelets_detail', bracelet_id=bracelet_id))
+
+
+@app.route('/bracelets/<bracelet_id>/edit')
+def bracelets_edit(bracelet_id):
+    """Show the edit form for a playlist."""
+    bracelet = bracelets.find_one({'_id': ObjectId(bracelet_id)})
+    # Add the title parameter here
+    return render_template('bracelets_edit.html', bracelet=bracelet, title='Edit bracelet')
 
 
 
